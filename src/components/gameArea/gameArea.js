f9771d718d0f9d093fedaf4551ad0aadcd7aa09d
gautamsharan <gautam.sharan@collabera.com>
2023-10-11 11:28:05 +0530

    tic tac toe

--- src/components/gameArea/gameArea.js
+++ src/components/gameArea/gameArea.js
@@ -3,15 +3,158 @@ import "./gameArea.css";
 
 //import components
 import GameSquare from "../gameSquare/gameSquare";
+import { useCallback } from "react";
 
 
-// this components can be used to represent your game area (up do you if you want to use it)
-function GameArea(props) {
- 
+function GameArea({ player1, player2, hardReset }) {
+  // we will use state to determin who's turn it is, and ofcourse who OWNS each square
+  const [turn, setTurn] = useState(true);
+  const [grid, setGrid] = useState([
+    { id: 0, owner: null },
+    { id: 1, owner: null },
+    { id: 2, owner: null },
+    { id: 3, owner: null },
+    { id: 4, owner: null },
+    { id: 5, owner: null },
+    { id: 6, owner: null },
+    { id: 7, owner: null },
+    { id: 8, owner: null },
+  ]);
+  const [winner, setWinner] = useState(null);
+  const [reset, setReset] = useState(true);
+
+  // this function changes the turn after every go
+  const changeTurn = () => {
+    setTurn(!turn);
+  };
+
+  // this function is used to mark the owner of the square, and update the grid state so it can be re-rendered
+  const markSquare = ({ id, owner }) => {
+    const data = { id, owner };
+
+    const newGrid = grid.map((square) => {
+      if (square.id === id) {
+        return data;
+      }
+      return square;
+    });
+    setGrid(newGrid);
+  };
+
+
+  // this is our win conditions and will be checked after each person has clicked
+  const checkWin = useCallback(() => {
+    if (
+      (grid[0].owner &&
+        grid[0].owner === grid[1].owner &&
+        grid[1].owner === grid[2].owner) ||
+      (grid[3].owner &&
+        grid[3].owner === grid[4].owner &&
+        grid[4].owner === grid[5].owner) ||
+      (grid[6].owner &&
+        grid[6].owner === grid[7].owner &&
+        grid[7].owner === grid[8].owner) ||
+      (grid[0].owner &&
+        grid[0].owner === grid[3].owner &&
+        grid[3].owner === grid[6].owner) ||
+      (grid[1].owner &&
+        grid[1].owner === grid[4].owner &&
+        grid[4].owner === grid[7].owner) ||
+      (grid[2].owner &&
+        grid[2].owner === grid[5].owner &&
+        grid[5].owner === grid[8].owner) ||
+      (grid[0].owner &&
+        grid[0].owner === grid[4].owner &&
+        grid[4].owner === grid[8].owner) ||
+      (grid[2].owner &&
+        grid[2].owner === grid[4].owner &&
+        grid[4].owner === grid[6].owner)
+    ) {
+      console.log("someone won", turn);
+      if (turn) {
+        setWinner(2);
+      } else {
+        setWinner(1);
+      }
+    } else {
+      let tieCheck = 0;
+      grid.forEach((square) => {
+        if (square.owner) {
+          tieCheck += 1;
+        }
+      });
+      if (tieCheck === grid.length) {
+        setWinner(3);
+      }
+    }
+  }, [grid, turn]);
+
+  useEffect(() => {
+    checkWin();
+  }, [checkWin]);
+
+  // this is to just reset the game board
+  const softReset = () => {
+    setGrid([
+      { id: 0, owner: null },
+      { id: 1, owner: null },
+      { id: 2, owner: null },
+      { id: 3, owner: null },
+      { id: 4, owner: null },
+      { id: 5, owner: null },
+      { id: 6, owner: null },
+      { id: 7, owner: null },
+      { id: 8, owner: null },
+    ]);
+    setWinner(null);
+    setTurn(true);
+    setReset(!reset);
+  };
+
+  const fullReset = () => {
+    softReset();
+    hardReset();
+  };
 
   return (
     <div id="gameArea">
-     <GameSquare/>
+      <h3>GAME ON!</h3>
+      <br />
+      {!winner ? (
+        <h4>{turn ? player1 : player2}'s turn</h4>
+      ) : winner === 3 ? (
+        <h4>TIE GAME</h4>
+      ) : (
+        <h4>{winner === 1 ? player1 : player2} WINS!</h4>
+      )}
+      <div className="gameBoard">
+        {grid.map((square) => {
+          return (
+            <GameSquare
+              id={square.id}
+              key={square.id}
+              changeTurn={changeTurn}
+              turn={turn}
+              markSquare={markSquare}
+              winner={winner}
+              reset={reset}
+            />
+          );
+        })}
+      </div>
+      <br />
+      {winner ? (
+        <div>
+          <button className="hardReset custom-btn" onClick={softReset}>
+            Play another Round?
+          </button>
+          <button className="softReset custom-btn" onClick={fullReset}>
+            Reset Game
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
--- src/components/gameSquare/gameSquare.js
+++ src/components/gameSquare/gameSquare.js
@@ -1,13 +1,33 @@
 import "./gameSquare.css";
 import { useState, useEffect } from "react";
 
-function GameSquare(props) {
- 
-// this component can be used to represent each square of the tic tac toe board
+function GameSquare({ id, changeTurn, turn, markSquare, winner, reset }) {
+  const [owner, setOwner] = useState(null);
+
+  useEffect(() => {
+    setOwner(null)
+  }, [reset])
+  // this will be used to see if a square has already been taken, and if not set an owner for it. The click will also trigger a turn change (as you can see from the bottom of the function)
+  const handleClick = () => {
+    if (!owner && !winner) {
+      if (turn) {
+        markSquare({ id, owner: 1 });
+        setOwner("X");
+      } else {
+        markSquare({ id, owner: 2 });
+        setOwner("O");
+      }
+      changeTurn();
+    }
+  };
+
   return (
     <div
+      className={`gameSquare square${id}`}
+      onClick={handleClick}
+      id={`square${id}`}
     >
-      <p>This is your game square component</p>
+      <p>{owner ? owner : ""}</p>
     </div>
   );
 }
